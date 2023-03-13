661e7ac43167c0babaf060a7300c16ff7dd10b45
AndresGarnica07 <marioasg1322@gmail.com>
2023-03-13 17:54:11 -0500

    Se agrega el proyecto

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+route::get('/categorias', 'CategoriesController@index' );
+
+
+Route::get('/', function () {
+    return view('principal');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('reply_thread', function () {
+    return view('reply_thread');
+});
+
+Route::get('create_thread', function () {
+    return view('create_thread');
+});
